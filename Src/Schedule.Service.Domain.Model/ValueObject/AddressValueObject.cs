0b54911c81f627f7e792b9803b71c7515e219f21
Guilherme Paschoal <guilhermeabpaschoal@gmail.com>
2021-12-12 09:41:52 -0300

    add address value object

---
+++ Src/Schedule.Service.Domain.Model/ValueObject/AddressValueObject.cs
@@ -0,0 +1,5 @@
+﻿using Schedule.Service.Domain.Model.Entities;
+
+namespace Schedule.Service.Domain.Model.ValueObject;
+
+public record AddressValueObject(string Street, string Neighborhood, string LocalReference, string ZipCode, string Number, City City);
