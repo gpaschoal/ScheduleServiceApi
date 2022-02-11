87131a8c365e14d5fb91b2ec1c3be5d598a841e8
Guilherme Paschoal <guilhermeabpaschoal@gmail.com>
2022-02-11 15:04:33 -0300

    Should be invalid when already exists a country with the same externalCode

--- Test/ScheduleService.Application.HandlerTest/Handlers/Countries/CountryUpdateHandlerTest.cs
+++ Test/ScheduleService.Application.HandlerTest/Handlers/Countries/CountryUpdateHandlerTest.cs
@@ -66,4 +66,25 @@ public class CountryUpdateHandlerTest
         countryUpdateRepositoryMock.Verify(x => x.ExistsCountryWithName(It.IsAny<Guid>(), It.IsAny<string>()), Times.Once);
         countryUpdateRepositoryMock.Verify(x => x.ExistsCountryWithExternalCode(It.IsAny<Guid>(), It.IsAny<string>()), Times.Once);
     }
+
+    [Fact(DisplayName = "Should be invalid when already exists a country with the same externalCode")]
+    public void Should_be_invalid_when_already_exists_a_country_with_the_same_externalCode()
+    {
+        var command = MakeValidCommand();
+        Mock<ICountryUpdateRepository> countryUpdateRepositoryMock = new();
+
+        var country = new Country(command.Name, command.ExternalCode);
+        countryUpdateRepositoryMock.Setup(x => x.GetByIdAsync(command.Id)).Returns(ValueTask.FromResult(country));
+        countryUpdateRepositoryMock.Setup(x => x.ExistsCountryWithExternalCode(command.Id, command.ExternalCode)).Returns(true);
+
+        var sut = MakeSut(countryUpdateRepositoryMock.Object);
+
+        var resultData = sut.Handle(command, CancellationToken.None).Result;
+
+        resultData.IsValid.Should().BeFalse();
+        resultData.Errors.Single().Key.Should().Be(nameof(command.ExternalCode));
+        countryUpdateRepositoryMock.Verify(x => x.UpdateAsync(It.IsAny<Country>()), Times.Never);
+        countryUpdateRepositoryMock.Verify(x => x.ExistsCountryWithName(It.IsAny<Guid>(), It.IsAny<string>()), Times.Once);
+        countryUpdateRepositoryMock.Verify(x => x.ExistsCountryWithExternalCode(It.IsAny<Guid>(), It.IsAny<string>()), Times.Once);
+    }
 }
