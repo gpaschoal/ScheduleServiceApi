02e24c563d32a1165c50f12ba1cd9793161732dc
Guilherme Paschoal <guilhermeabpaschoal@gmail.com>
2022-02-11 08:12:43 -0300

    fix handle base tests

--- Test/ScheduleService.Application.HandlerTest/Handlers/HandlerBaseTests.cs
+++ Test/ScheduleService.Application.HandlerTest/Handlers/HandlerBaseTests.cs
@@ -1,117 +1,87 @@
-﻿namespace ScheduleService.Application.HandlerTest.Handlers;
+﻿using FluentAssertions;
+using Moq;
+using ScheduleService.Application.Shared;
+using System.Linq;
+using Xunit;
+
+namespace ScheduleService.Application.HandlerTest.Handlers;
 
 public partial class HandlerBaseTests
 {
-    //private static HandlerBaseStub MakeSut(IHandlerBus? handlerBus = null)
-    //{
-    //    handlerBus ??= new Mock<IHandlerBus>().Object;
+    private static HandlerBaseStub MakeSut() => new();
 
-    //    return new(handlerBus);
-    //}
 
-    //[Fact(DisplayName = "HandlerBase should be valid when is created")]
-    //public void HandlerBase_should_be_valid_when_is_created_and_invalid_after_add_an_error()
-    //{
-    //    var sut = MakeSut();
+    [Fact(DisplayName = "HandlerBase should be valid when is created")]
+    public void HandlerBase_should_be_valid_when_is_created_and_invalid_after_add_an_error()
+    {
+        var sut = MakeSut();
 
-    //    sut.IsValid.Should().BeTrue();
-    //    sut.ValidResponseAsync().IsValid.Should().BeTrue();
-    //    sut.InvalidResponseAsync().IsValid.Should().BeTrue();
-    //    sut.ValidResponse().Result.IsValid.Should().BeTrue();
-    //    sut.InvalidResponse().Result.IsValid.Should().BeTrue();
-    //    sut.IsInvalid.Should().BeFalse();
+        sut.IsInvalid.Should().BeFalse();
+        sut.ValidResponseAsync().Result.IsValid.Should().BeTrue();
+        sut.InvalidResponseAsync().Result.IsValid.Should().BeTrue();
+        sut.ValidResponse().IsValid.Should().BeTrue();
+        sut.InvalidResponse().IsValid.Should().BeTrue();
+        sut.IsInvalid.Should().BeFalse();
 
-    //    string key = "_key";
-    //    sut.AddError(key, "this key is invalid");
+        string key = "_key";
+        sut.AddError(key, "this key is invalid");
 
-    //    sut.IsValid.Should().BeFalse();
-    //    sut.ValidResponseAsync().IsValid.Should().BeTrue();
-    //    sut.IsInvalid.Should().BeTrue();
-    //    sut.InvalidResponseAsync().IsValid.Should().BeFalse();
-    //    sut.ValidResponse().Result.IsValid.Should().BeTrue();
-    //    sut.InvalidResponse().Result.IsValid.Should().BeFalse();
-    //    sut.HandleExecutionCalls.Should().Be(0);
-    //}
+        sut.IsInvalid.Should().BeTrue();
+        sut.ValidResponseAsync().Result.IsValid.Should().BeTrue();
+        sut.IsInvalid.Should().BeTrue();
+        sut.InvalidResponseAsync().Result.IsValid.Should().BeFalse();
+        sut.ValidResponse().IsValid.Should().BeTrue();
+        sut.InvalidResponse().IsValid.Should().BeFalse();
+        sut.HandleExecutionCalls.Should().Be(0);
+    }
 
-    //[Fact(DisplayName = "Handle should not execute HandleExecution when command is Invalid")]
-    //public void Handle_should_not_execute_HandleExecution_when_command_is_Invalid()
-    //{
-    //    CustomResultData invalidDataResult = new();
-    //    invalidDataResult.IsValid.Should().BeTrue();
-    //    invalidDataResult.AddFieldError("_key", "this is a message");
-    //    invalidDataResult.IsValid.Should().BeFalse();
+    [Fact(DisplayName = "Handle should execute HandleExecution when command is Valid")]
+    public void Handle_should_execute_HandleExecution_when_command_is_Valid()
+    {
+        CustomResultData invalidDataResult = new();
+        invalidDataResult.IsValid.Should().BeTrue();
 
-    //    var validatorLocatorMock = new Mock<IValidatorLocator>();
-    //    validatorLocatorMock.Setup(x => x.ValidateCommand(It.IsAny<StubCommand>())).Returns(invalidDataResult);
+        var sut = MakeSut();
 
-    //    var handlerBusMock = new Mock<IHandlerBus>();
-    //    handlerBusMock.Setup(x => x.Validator).Returns(validatorLocatorMock.Object);
+        CustomResultData commandResult = sut.Handle(It.IsAny<StubCommand>(), System.Threading.CancellationToken.None).Result;
 
-    //    var sut = MakeSut(handlerBusMock.Object);
+        commandResult.IsValid.Should().BeTrue();
+        sut.HandleExecutionCalls.Should().Be(1);
+    }
 
-    //    CustomResultData commandResult = sut.Handle(It.IsAny<StubCommand>(), System.Threading.CancellationToken.None).Result;
+    [Fact(DisplayName = "AddError should add errors to the intern Response")]
+    public void AddError_should_add_errors_to_the_intern_Response()
+    {
+        var sut = MakeSut();
 
-    //    commandResult.IsValid.Should().BeFalse();
-    //    sut.HandleExecutionCalls.Should().Be(0);
-    //}
+        sut.IsInvalid.Should().BeFalse();
 
-    //[Fact(DisplayName = "Handle should execute HandleExecution when command is Valid")]
-    //public void Handle_should_execute_HandleExecution_when_command_is_Valid()
-    //{
-    //    CustomResultData invalidDataResult = new();
-    //    invalidDataResult.IsValid.Should().BeTrue();
+        string key = "_key";
+        sut.AddError(key, "this key is invalid");
 
-    //    var validatorLocatorMock = new Mock<IValidatorLocator>();
-    //    validatorLocatorMock.Setup(x => x.ValidateCommand(It.IsAny<StubCommand>())).Returns(invalidDataResult);
+        sut.IsInvalid.Should().BeTrue();
 
-    //    var handlerBusMock = new Mock<IHandlerBus>();
-    //    handlerBusMock.Setup(x => x.Validator).Returns(validatorLocatorMock.Object);
+        var invalidCommandResult = sut.InvalidResponseAsync().Result;
+        invalidCommandResult.IsValid.Should().BeFalse();
 
-    //    var sut = MakeSut(handlerBusMock.Object);
+        invalidCommandResult.Errors.Single().Key.Should().Be(key);
+    }
 
-    //    CustomResultData commandResult = sut.Handle(It.IsAny<StubCommand>(), System.Threading.CancellationToken.None).Result;
+    [Fact(DisplayName = "AddError should add message errors to the intern Response")]
+    public void AddError_should_add_message_errors_to_the_intern_Response()
+    {
+        var sut = MakeSut();
 
-    //    commandResult.IsValid.Should().BeTrue();
-    //    sut.HandleExecutionCalls.Should().Be(1);
-    //}
+        sut.IsInvalid.Should().BeFalse();
 
-    //[Fact(DisplayName = "AddError should add errors to the intern Response")]
-    //public void AddError_should_add_errors_to_the_intern_Response()
-    //{
-    //    var sut = MakeSut();
+        string message = "this message is invalid";
+        sut.AddError(message);
 
-    //    sut.IsValid.Should().BeTrue();
-    //    sut.IsInvalid.Should().BeFalse();
+        sut.IsInvalid.Should().BeTrue();
 
-    //    string key = "_key";
-    //    sut.AddError(key, "this key is invalid");
+        var invalidCommandResult = sut.InvalidResponseAsync().Result;
+        invalidCommandResult.IsValid.Should().BeFalse();
 
-    //    sut.IsValid.Should().BeFalse();
-    //    sut.IsInvalid.Should().BeTrue();
-
-    //    var invalidCommandResult = sut.InvalidResponseAsync();
-    //    invalidCommandResult.IsValid.Should().BeFalse();
-
-    //    invalidCommandResult.FieldErrors.Single().Key.Should().Be(key);
-    //}
-
-    //[Fact(DisplayName = "AddError should add message errors to the intern Response")]
-    //public void AddError_should_add_message_errors_to_the_intern_Response()
-    //{
-    //    var sut = MakeSut();
-
-    //    sut.IsValid.Should().BeTrue();
-    //    sut.IsInvalid.Should().BeFalse();
-
-    //    string message = "this message is invalid";
-    //    sut.AddError(message);
-
-    //    sut.IsValid.Should().BeFalse();
-    //    sut.IsInvalid.Should().BeTrue();
-
-    //    var invalidCommandResult = sut.InvalidResponseAsync();
-    //    invalidCommandResult.IsValid.Should().BeFalse();
-
-    //    invalidCommandResult.MessageErrors.Single().Should().Be(message);
-    //}
+        invalidCommandResult.Errors.SelectMany(x => x.Value).Single().Should().Be(message);
+    }
 }
