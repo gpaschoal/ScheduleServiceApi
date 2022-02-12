8b9a6a3884ee786538aac34f116193b257623558
Guilherme Paschoal <guilhermeabpaschoal@gmail.com>
2022-02-11 21:20:33 -0300

    Should be invalid when does not exists the countryId

--- Test/ScheduleService.Application.HandlerTest/Handlers/States/StateCreateHandlerTest.cs
+++ Test/ScheduleService.Application.HandlerTest/Handlers/States/StateCreateHandlerTest.cs
@@ -29,9 +29,9 @@ public class StateCreateHandlerTest
     {
         StateCreateCommand command = new() { };
 
-        Mock<IStateCreateRepository> StateCreateRepositoryMock = new();
+        Mock<IStateCreateRepository> stateCreateRepositoryMock = new();
 
-        var sut = MakeSut(StateCreateRepositoryMock.Object);
+        var sut = MakeSut(stateCreateRepositoryMock.Object);
 
         var resultData = sut.Handle(command, CancellationToken.None).Result;
 
@@ -40,29 +40,31 @@ public class StateCreateHandlerTest
         resultData.Errors.Should().Contain(x => x.Key == nameof(command.Name));
         resultData.Errors.Should().Contain(x => x.Key == nameof(command.ExternalCode));
 
-        StateCreateRepositoryMock.Verify(x => x.ExistsStateWithName(It.IsAny<string>()), Times.Never);
-        StateCreateRepositoryMock.Verify(x => x.ExistsStateWithExternalCode(It.IsAny<string>()), Times.Never);
-        StateCreateRepositoryMock.Verify(x => x.AddAsync(It.IsAny<State>()), Times.Never);
+        stateCreateRepositoryMock.Verify(x => x.ExistsStateWithName(It.IsAny<string>()), Times.Never);
+        stateCreateRepositoryMock.Verify(x => x.ExistsStateWithExternalCode(It.IsAny<string>()), Times.Never);
+        stateCreateRepositoryMock.Verify(x => x.AddAsync(It.IsAny<State>()), Times.Never);
+        stateCreateRepositoryMock.Verify(x => x.CheckIfCountryExists(command.CountryId), Times.Never);
     }
 
     [Fact(DisplayName = "Should be invalid when already exists a state with the same name")]
     public void Should_be_invalid_when_already_exists_a_state_with_the_same_name()
     {
         var command = MakeValidCommand();
-        Mock<IStateCreateRepository> countryCreateRepositoryMock = new();
+        Mock<IStateCreateRepository> stateCreateRepositoryMock = new();
 
-        countryCreateRepositoryMock.Setup(x => x.ExistsStateWithName(command.Name)).Returns(true);
-        countryCreateRepositoryMock.Setup(x => x.CheckIfCountryExists(command.CountryId)).Returns(ValueTask.FromResult(true));
+        stateCreateRepositoryMock.Setup(x => x.ExistsStateWithName(command.Name)).Returns(true);
+        stateCreateRepositoryMock.Setup(x => x.CheckIfCountryExists(command.CountryId)).Returns(ValueTask.FromResult(true));
 
-        var sut = MakeSut(countryCreateRepositoryMock.Object);
+        var sut = MakeSut(stateCreateRepositoryMock.Object);
 
         var resultData = sut.Handle(command, CancellationToken.None).Result;
 
         resultData.IsValid.Should().BeFalse();
         resultData.Errors.Single().Key.Should().Be(nameof(command.Name));
-        countryCreateRepositoryMock.Verify(x => x.AddAsync(It.IsAny<State>()), Times.Never);
-        countryCreateRepositoryMock.Verify(x => x.ExistsStateWithName(command.Name), Times.Once);
-        countryCreateRepositoryMock.Verify(x => x.ExistsStateWithExternalCode(command.ExternalCode), Times.Once);
+        stateCreateRepositoryMock.Verify(x => x.AddAsync(It.IsAny<State>()), Times.Never);
+        stateCreateRepositoryMock.Verify(x => x.ExistsStateWithName(command.Name), Times.Once);
+        stateCreateRepositoryMock.Verify(x => x.ExistsStateWithExternalCode(command.ExternalCode), Times.Once);
+        stateCreateRepositoryMock.Verify(x => x.CheckIfCountryExists(command.CountryId), Times.Once);
     }
 
     [Fact(DisplayName = "Should be invalid when already exists a state with the same externalCode")]
@@ -83,6 +85,25 @@ public class StateCreateHandlerTest
         stateCreateRepositoryMock.Verify(x => x.AddAsync(It.IsAny<State>()), Times.Never);
         stateCreateRepositoryMock.Verify(x => x.ExistsStateWithName(command.Name), Times.Once);
         stateCreateRepositoryMock.Verify(x => x.ExistsStateWithExternalCode(command.ExternalCode), Times.Once);
+        stateCreateRepositoryMock.Verify(x => x.CheckIfCountryExists(command.CountryId), Times.Once);
+    }
+
+    [Fact(DisplayName = "Should be invalid when does not exists the countryId")]
+    public void Should_be_invalid_when_does_not_exists_the_countryId()
+    {
+        var command = MakeValidCommand();
+        Mock<IStateCreateRepository> stateCreateRepositoryMock = new();
+        stateCreateRepositoryMock.Setup(x => x.CheckIfCountryExists(command.CountryId)).Returns(ValueTask.FromResult(false));
+
+        var sut = MakeSut(stateCreateRepositoryMock.Object);
+
+        var resultData = sut.Handle(command, CancellationToken.None).Result;
+
+        resultData.IsValid.Should().BeFalse();
+        stateCreateRepositoryMock.Verify(x => x.AddAsync(It.IsAny<State>()), Times.Never);
+        stateCreateRepositoryMock.Verify(x => x.ExistsStateWithName(command.Name), Times.Once);
+        stateCreateRepositoryMock.Verify(x => x.ExistsStateWithExternalCode(command.ExternalCode), Times.Once);
+        stateCreateRepositoryMock.Verify(x => x.CheckIfCountryExists(command.CountryId), Times.Once);
     }
 
     [Fact(DisplayName = "Should add a state")]
